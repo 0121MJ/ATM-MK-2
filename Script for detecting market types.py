b582335f60d33ff873bdda9021914182086a32e7
James Sharp <84133470+jameslbsharp@users.noreply.github.com>
2022-02-10 14:15:13 +0000

    Update Script for detecting market types.py
    If a company registers as small cap, the script now detects whether or not it is in the index of small cap firms (if not it tells the user it is the right size but not found)
    
    The script now details a progress report as it finds each firm, instead of just a blank screen

--- Script for detecting market types.py
+++ Script for detecting market types.py
@@ -24,15 +24,29 @@ response = requests.get("https://www.sharecast.com/index/FTSE_Small_Cap")
 soup = BeautifulSoup(response.text, 'lxml')
 market_cap = soup.find_all("td", class_="text-right xs-col-2 nth-order-5 capitalization-ttl")
 
-thing = []
+sc_firms_marketcap = []
 
 for i in market_cap:
     entry = i.get_text().replace('\n','').replace('£','').replace('m','').strip()
-    thing.append(float(entry))
+    sc_firms_marketcap.append(float(entry))
 
-largest_firm = max(thing)
-print(f"The largest SmallCap firm is worth £{largest_firm}m")
+largest_firm_cap = max(sc_firms_marketcap)
+
+#this webpage isn't as reliable as the previous so a try/except is used here
+try:
+    response = requests.get("https://www.sharecast.com/index/FTSE_Small_Cap/market-capitalization/desc")
+    soup = BeautifulSoup(response.text, 'lxml')
+    company_names = soup.find_all("th", class_="xs-col-3 nth-order-1 ttl")
+    top_sc_firm_name = company_names[0].get_text().replace('\n','').strip()
+    top_sc_firm_name
+except:
+    top_sc_firm_name = "*NOT FOUND*"
+    pass
+
+print(f"Script finds largest SmallCap firm is *{top_sc_firm_name}* at £{largest_firm_cap}m")
 ####
+
+print("\nScript now searching for firms on LSE website...") 
 
 search_error_counter = 0
 
@@ -91,6 +105,7 @@ while a < len(col): # iterates through the companies the user uploaded
                                    
     # sometimes a valid company gets caught on the search page, in which case this makes it retry
     if driver.current_url[37] == "e" and search_error_counter < 3: # this is a characteristic of the search page, part of forcing it to retry
+        print(f"Error finding *{company_name}*, retrying {search_error_counter+1}/3 attempts")
         search_error_counter += 1
         url.pop()
         found = True
@@ -99,24 +114,46 @@ while a < len(col): # iterates through the companies the user uploaded
     else:
         a += 1
         
-    found = True
-        
-    print(driver.current_url)
-    
+    found = True    
+    print(f"Found firm {a}/{len(col)}")
     driver.close()
-    
     search_error_counter = 0
 
-    
-print("The URLs for the companies on the LSE website were:")
+print("\n")
+print("\nFirms LSE website pages identified")    
+print("\nThe URLs for the companies on the LSE website were:")
 for a in url:
-    print(a)
-    
+    if a == "https://www.blank.org/":
+        print("*NOT FOUND*")
+    else:
+        print(a)
+
+print("\nNow determining market types...")
+
+
+### this routine verifies that a detected smallcap firm is indeed found on the index or not
+#using the official LSE index, not used elsewhere because it doesn't store the market caps
+SC_index = ["https://www.hl.co.uk/shares/stock-market-summary/ftse-small-cap?page=1",
+            "https://www.hl.co.uk/shares/stock-market-summary/ftse-small-cap?page=2",
+            "https://www.hl.co.uk/shares/stock-market-summary/ftse-small-cap?page=3"]
+smallcap_firm_list = []
+
+for l in SC_index:
+    response = requests.get(l)
+    soup = BeautifulSoup(response.text, 'lxml')
+    firm_name = soup.find_all("td", class_="name-col align-left")
+
+    for m in firm_name:
+        smallcap_firm_list.append(m.get_text().lower())
+###
+
+
 # this section detects which companies are listed as AIM or Main Market, now their URLs are collected
 book = []
 record = []
 counter = 0
 
+        
 for j in url:
     
     for k in range(6): # five attempts of getting the market type before giving up
@@ -135,8 +172,18 @@ for j in url:
         elif " Market Main Market" in book:
             collection = soup.find_all("div", class_="bold-font-weight regular-font-size")
             company_mc = float(collection[5].getText().replace(',',''))
-            if company_mc <= largest_firm:
-                record.append("Small Cap")
+            
+            if company_mc <= largest_firm_cap:
+                #this gets the name of the current firm from the URL and checks if it's in the list of SC firms
+                potential_smallcap = j.replace("/company-page",'')
+                potential_smallcap = potential_smallcap.split('/')
+                potential_smallcap = potential_smallcap[5]
+                potential_smallcap = potential_smallcap.replace('-',' ').lower()
+                
+                if potential_smallcap in smallcap_firm_list:
+                    record.append("SmallCap")
+                else:
+                    record.append("SmallCap size but couldn't find in SmallCap index")
             else:
                 record.append("Main Market")
             break
@@ -150,7 +197,11 @@ for j in url:
         print(f"Company {counter} not found, {k}/5 attempts")
         
     counter += 1
+    print(f"Found {counter}/{len(col)} market types")
 
 print(record)
 df = pd.DataFrame(record)
 df.to_excel("output_file.xlsx")
+
+print("\n")
+print("\nScript complete!")
